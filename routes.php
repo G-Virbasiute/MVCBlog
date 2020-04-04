ab2a9c550e48d4d0d8883fedd3d625b87bf9a1fc
Sandy Forrester <sandyforresteruk@gmail.com>
2020-04-04 19:55:51 +0100

    Updated routes file to include users and posts controllers

--- routes.php
+++ routes.php
@@ -25,8 +25,9 @@
 // for validation we list the allowed controllers and their actions
 // Add an entry for each new controller and its actions
 $controllers = array('pages' => ['home', 'error'],
-                    'product' => ['readAll','read','create','update','delete'],
+                    'user' => ['readAllUsers','readUser','createUser','updateUser','deleteUser'],
                     'post' => ['readAll', 'read', 'create', 'update', 'delete'],
+                    'controllerXXX' => ['actionYYY', 'actionZZZ'],
                     );
 
   // check that the requested controller and action are both allowed
