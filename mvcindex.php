f3685fc6587b7ec8c4b16bd930384eb38127a26b
Gabriele Virbasiute <g.virbasiute@gmail.com>
2020-04-04 20:14:30 +0100

    change the connection file and layout location.

--- mvcindex.php
+++ mvcindex.php
@@ -6,7 +6,7 @@
     </head>
     <body>
         <?php
-    require_once('blogconnection.php');
+    require_once('connection.php');
         
     if (isset($_GET['controller']) && isset($_GET['action'])) {
         $controller = $_GET['controller'];
@@ -16,7 +16,7 @@
         $action     = 'home';
   }
 
-    require_once('views/layout.php');
+    require_once('layout.php');
         ?>
     </body>
 </html>
