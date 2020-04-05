38a3f9e4c5f1992c2e38adf3b2a281da1d757ac6
supersquamy <amyrobinsonlearns@gmail.com>
2020-04-05 13:36:26 +0100

    Tweaked 'difficulty rating' on 'create blog post' form to be a dropdown menu

--- views/posts/create.php
+++ views/posts/create.php
@@ -1,37 +1,40 @@
 <p>Fill in the following form to create a new blog post:</p>
 <form action="" method="POST" class="w3-container" enctype="multipart/form-data">
-    
+
     <h2>Create New Blog Post</h2>
 </div>
 <p>
-        <input class="w3-input" type="text" name="userid" required autofocus>
-        <label>User ID</label>
-    </p>
-    <p>
-        <input class="w3-input" type="text" name="title" required autofocus>
-        <label>Title</label>
-    </p>
-        <p>
-        <input class="w3-input" type="text" name="blurb" required>
-        <label>Blurb</label>
-    </p>
-    <p>
-        <input class="w3-input" type="text" name="content" required>
-        <label>Content</label>
-    </p>
-    <p>
-        <input class="w3-input" type="text" name="rating" required>
-        <label>Difficulty rating (Beginner/Intermediate/Expert)</label>
-    </p>
-    
-            
- <!-- <input type="hidden"  -->
+    <input class="w3-input" type="text" name="userid" required autofocus>
+    <label>User ID</label>
+</p>
+<p>
+    <input class="w3-input" type="text" name="title" required autofocus>
+    <label>Title</label>
+</p>
+<p>
+    <input class="w3-input" type="text" name="blurb" required>
+    <label>Blurb</label>
+</p>
+<p>
+    <input class="w3-input" type="text" name="content" required>
+    <label>Content</label>
+</p>
+
+<label>Difficulty rating:</label>
+<select name="rating" required>
+    <option><option value="Beginner">Beginner</option>
+    <option><option value="Intermediate">Intermediate</option>
+    <option><option value="Expert">Expert</option>
+</select>
+
+
+<!-- <input type="hidden"  -->
 <!--	   name="MAX_FILE_SIZE"  -->
 <!--         value="10000000"  -->
 <!--         />  -->
 
   <!--<input type="file" name="myUploader" class="w3-btn w3-pink" required />-->
-  <p>
+<p>
     <input class="w3-btn w3-pink" type="submit" value="Create My Post!">
-  </p>
+</p>
 </form>
