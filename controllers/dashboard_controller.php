ef20cf6f4b24e1cf49b964bf04b305a9d428cd95
Maria <mariaflanagan1@googlemail.com>
2020-04-07 14:08:52 +0100

    Update dashboard_controller.php

--- controllers/dashboard_controller.php
+++ controllers/dashboard_controller.php
@@ -8,47 +8,20 @@ class DashboardController {
       require_once('views/dashboard/read.php');
     }
 
-    public function create() {
-        // we expect a url of form ?controller=products&action=create
-        // if it's a GET request display a blank form for creating a new blog post
-        // else it's a POST so add to the database and redirect to readAll action
-        if($_SERVER['REQUEST_METHOD'] == 'GET'){
-            //require_once('views/posts/create.php');
-        }
-        else { 
-            Category::add();
-             
-            $categories = Category::all(); //$posts is used within the view
-            //require_once('views/posts/readAll.php');
-        }
+    public function createPost() {
+        // todo
     }
-    public function update() {
-        
-      if($_SERVER['REQUEST_METHOD'] == 'GET'){
-          if (!isset($_GET['id']))
-        return call('pages', 'error');
-
-        // we use the given id to get the correct post
-        $category = Category::find($_GET['id']);
-      
-        //require_once('views/posts/update.php');
-        }
-      else
-          { 
-            $id = $_GET['id'];
-            Category::update($id);
-                        
-            $categories = Category::all();
-            //require_once('views/posts/readAll.php');
-      }
-      
+    
+    public function updateDetails() {
+        // todo
     }
     public function delete() {
-            Category::remove($_GET['id']);
-            
-            $categories = Category::all();
-            //require_once('views/posts/readAll.php');
-      }
+        // todo
+    }
+    
+    public function deletePost() {
+        // todo
+    }
       
     }
   
