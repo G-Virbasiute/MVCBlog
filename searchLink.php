4eb5c4adf61c6685ab26104dbd85562f3dde2e38
supersquamy <amyrobinsonlearns@gmail.com>
2020-04-11 19:56:27 +0100

    Revert "Changed search redirect link to be for hosted site rather than local site"
    This reverts commit ec42d3e567f5c03848d710760686b6e51d220d62.

--- searchLink.php
+++ searchLink.php
@@ -3,6 +3,7 @@
    if(isset($_GET['search']))
       $query = $_GET['search'];
 
-       header("Location: https://lifesastitch.uk/mvcindex.php?controller=post&action=search&search={$query}");
+       header("Location: http://localhost/FINAL_PROJECT_MVC/mvcindex.php?controller=post&action=search&search={$query}");
+
 
 ?>
