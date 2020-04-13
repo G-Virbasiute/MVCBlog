41a3c9e31bb3654e603c823ef5775476df50b64c
Sandy Forrester <sandyforresteruk@gmail.com>
2020-04-13 18:31:05 +0100

    Tweaked update user and user picture views to get navbar working

--- views/users/updatepicture.php
+++ views/users/updatepicture.php
@@ -1,15 +1,3 @@
-<html>
-    <head>
-        <title>Update user picture</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css?family=Amatic+SC&display=swap" rel="stylesheet">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
-        <link href="carouselcss.css" rel="stylesheet" type="text/css"/>
-    </head>
     <body style="font-family: 'Amatic SC', cursive; font-size: 30px;">
         <div class="wrapper" style="width:350px;padding-left:20px;padding-top:20px;">
             <form action="" method="post" enctype="multipart/form-data">
