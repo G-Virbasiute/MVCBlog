beaa7e590399340dd9bdb9a7e8e5757ba990914e
Gabriele Virbasiute <g.virbasiute@gmail.com>
2020-04-17 11:40:53 +0100

    Added a privacy policy.

--- views/pages/privacy.php
+++ views/pages/privacy.php
@@ -1,6 +1,27 @@
 <center>
-    <p style="margin-left: 20px; font-size: 30px; font-family: 'Amatic SC', cursive;">Our privacy policy goes here...</p><br>
-    <img src='views/images/crochet.png' alt=" " width="30px" height="30px">
-    <img src='views/images/embroidery.png' alt=" " width="30px" height="30px">
-    <img src='views/images/knitting.png' alt=" " width="30px" height="30px">
+    <h1 style="margin-left: 20px; font-family: 'Amatic SC', cursive;">Our privacy policy</h1>
+    <img style="margin-left: 20px" src='views/images/crochet.png' alt=" " width="30px" height="30px">
+    <img style="margin-left: 20px" src='views/images/embroidery.png' alt=" " width="30px" height="30px">
+    <img style="margin-left: 20px" src='views/images/knitting.png' alt=" " width="30px" height="30px">
 </center>
+
+<div style="margin-left: 20px; width: 50%">
+<h1 style="width: 20%; border-bottom: 2px solid black; font-family: 'Amatic SC', cursive;">Who we are</h1>
+<p>Our website address is http://lifesastitch.uk/mvcindex.php</p>
+<p></p>
+
+<h1 style="width: 62%; border-bottom: 2px solid black; font-family: 'Amatic SC', cursive;">What personal data we collect and why</h1>
+<p></p>
+<h3 style="margin-left: 20px;">Comments</h3>
+<p>When users leave comments on the site we collect the data shown in the comments form, and also user's location (if shared) so we are able to display where our users are commenting from.</p>
+<p>This will not be shared with any third parties.</p>
+<h3 style="margin-left: 20px;">Media</h3>
+<p>If you upload images to the website, you should avoid uploading images with embedded location data included. Visitors to the website can download and extract any location data from the images on the website.</p>
+<p>This will not be shared with any third parties.</p>
+<h3 style="margin-left: 20px;">Contact and Suggestion Forms</h3>
+<p>The information submitted in these forms will only be accesible to the website's administrators. Your e-mail will only be used to send you a response if applicable.</p>
+<p>This will not be shared with any third parties.</p>
+<h3 style="margin-left: 20px;">Cookies</h3>
+<p>Your location information is stored in a cookie.</p>
+<p>This will not be shared with any third parties.</p>
+</div>
